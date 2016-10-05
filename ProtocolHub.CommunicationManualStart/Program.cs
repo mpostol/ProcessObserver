cbea8356f116f71176e2dcfb0cae260f3d695458
Mariusz <mpostol@cas.eu>
2016-10-05 09:52:07 +0000

    CAS.CommServer.ProtocolHub - rearrange - initial refactoring  - moving ProtocolHub to the destination folder. Issue #: 4860

---
+++ ProtocolHub.CommunicationManualStart/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CAS.OpcSvr.Da.NETServer;
+
+namespace CAS.CommServer.ProtocolHub.CommunicationManualStart
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+      try
+      {
+        CAS.Lib.CodeProtect.LibInstaller.InstalLicense();
+      }
+      catch ( CAS.Lib.CodeProtect.LicenseDsc.LicenseFileException ex )
+      {
+        Console.WriteLine( "Cannot install license, error:" + ex.Message );
+      }
+      Console.WriteLine( "CommServer is starting..... " );
+      DaServer daserver = new DaServer();
+      Console.WriteLine( "CommServer is started. Hit Enter to close application" );
+      Console.ReadLine();
+      Console.WriteLine( "Enter is receive, waiting for application end" );
+    }
+  }
+}
