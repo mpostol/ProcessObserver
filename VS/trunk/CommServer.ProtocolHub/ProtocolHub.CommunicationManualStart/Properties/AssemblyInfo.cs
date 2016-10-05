b8406bb720b2a120d93fa9a28dd4a6c2d40fee97
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-10-05 09:52:07 +0000

    CAS.CommServer.ProtocolHub - rearrange - initial refactoring  - moving ProtocolHub to the destination folder. Issue #: 4860

---
+++ VS/trunk/CommServer.ProtocolHub/ProtocolHub.CommunicationManualStart/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.CommServer.ProtocolHub.CommunicationManualStart
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.CommServer.ProtocolHub.CommunicationManualStart")]
+[assembly: AssemblyDescription("CommServer ProtocolHub Manual Start Development Tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("9bddd606-fe9f-4f06-9b52-fe0dad8f91cc")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("4.00.22.*")]
+[assembly: AssemblyFileVersion("4.00.22")]
+
