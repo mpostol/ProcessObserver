ce263b0351244c89bf8c290188397008bb7085f3
Mariusz <mpostol@cas.eu>
2017-08-10 21:08:10 +0000

    CommServer.DA.Server - create installation package - working on ProtocolHub.ConfiguartionEditor - fixed problem with foreach null exception - added try to the starting method - released new nuget 4.10.04 Issue #: 4862

--- ProtocolHub.ConfiguartionEditor/MainClass.cs
+++ ProtocolHub.ConfiguartionEditor/MainClass.cs
@@ -31,26 +31,40 @@ namespace CAS.CommServer.ProtocolHub.ConfigurationEditor
     [STAThread]
     static void Main()
     {
-      AssemblyTraceEvent.Tracer.TraceMessage(TraceEventType.Verbose, 32, "Starting application CAS.CommServer.ProtocolHub.ConfigurationEditor");
-      string m_CommandLine = Environment.CommandLine;
-      if (m_CommandLine.ToLower().Contains("installic"))
-        try
-        {
-          LibInstaller.InstallLicense(false);
-        }
-        catch (LicenseFileException ex)
-        {
-          MessageBox.Show("Cannot install license, error:" + ex.Message);
-        }
-      Application.Run
-        (
-          new ConfigTreeView(ConfigurationManagement.ProtocolHubConfiguration,
-                             new ConfigIOHandler(ConfigurationManagement.ReadConfiguration),
-                             new ConfigIOHandler(ConfigurationManagement.SaveProc),
-                             new ConfigIOHandler(ConfigurationManagement.ClearConfig),
-                             Settings.Default.ToolsMenu)
-         );
-      AssemblyTraceEvent.Tracer.TraceMessage(TraceEventType.Verbose, 32, "Finishing application CAS.CommServer.ProtocolHub.ConfigurationEditor");
+      try
+      {
+        AssemblyTraceEvent.Tracer.TraceMessage(TraceEventType.Verbose, 32, "Starting application CAS.CommServer.ProtocolHub.ConfigurationEditor");
+        string m_CommandLine = Environment.CommandLine;
+        if (m_CommandLine.ToLower().Contains("installic"))
+          try
+          {
+            LibInstaller.InstallLicense(false);
+          }
+          catch (LicenseFileException ex)
+          {
+            MessageBox.Show("Cannot install license, error:" + ex.Message);
+          }
+        Application.Run
+          (
+            new ConfigTreeView(ConfigurationManagement.ProtocolHubConfiguration,
+                               new ConfigIOHandler(ConfigurationManagement.ReadConfiguration),
+                               new ConfigIOHandler(ConfigurationManagement.SaveProc),
+                               new ConfigIOHandler(ConfigurationManagement.ClearConfig),
+                               Settings.Default.ToolsMenu)
+           );
+        AssemblyTraceEvent.Tracer.TraceMessage(TraceEventType.Verbose, 32, "Finishing application CAS.CommServer.ProtocolHub.ConfigurationEditor");
+      }
+      catch (Exception _ex)
+      {
+        string _message = $"The application has been finished by the exception {_ex.Message} call the vendor for assistance";
+        AssemblyTraceEvent.Tracer.TraceMessage(TraceEventType.Error, 36, _message);
+        AssemblyTraceEvent.Tracer.TraceMessage(TraceEventType.Error, 36, $"Stock for the exception {_ex.StackTrace}");
+        MessageBox.Show(_message, "Application error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+      finally
+      {
+        AssemblyTraceEvent.Tracer.TraceSource.Flush();
+      }
     }
   }
 }
