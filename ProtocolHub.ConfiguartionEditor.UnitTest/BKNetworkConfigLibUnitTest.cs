e2a1b63fccd8fc2236857221e4b40fe24130bf24
mpostol <mpostol@cas.eu>
2020-03-14 21:45:06 +0100

    DAServerConfiguration - remove dependency on `System.Windows.Forms` #16
    - fixes #16

---
+++ ProtocolHub.ConfiguartionEditor.UnitTest/BKNetworkConfigLibUnitTest.cs
@@ -0,0 +1,46 @@
+﻿//___________________________________________________________________________________
+//
+//  Copyright (C) 2020, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
+//___________________________________________________________________________________
+
+using CAS.CommServer.ProtocolHub.ConfigurationEditor.Components;
+using CAS.NetworkConfigLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UAOOI.ProeessObserver.ConfiguartionEditor
+{
+  [TestClass]
+  public class BKNetworkConfigLibUnitTest
+  {
+    [TestMethod]
+    public void CommServerConfigurationMainCreatorTest()
+    {
+      using (CommServerConfigurationMain _new = new CommServerConfigurationMain())
+      {
+        using (ComunicationNet _defaultCommunicationNet = _new.Configuartion)
+        {
+          Assert.IsNotNull(_defaultCommunicationNet);
+          Assert.IsNotNull(_defaultCommunicationNet.Channels);
+          Assert.IsNotNull(_defaultCommunicationNet.DataBlocks);
+          Assert.IsNotNull(_defaultCommunicationNet.Groups);
+          Assert.IsNotNull(_defaultCommunicationNet.Interfaces);
+          Assert.IsNotNull(_defaultCommunicationNet.ItemPropertiesTable);
+          Assert.IsNotNull(_defaultCommunicationNet.Protocol);
+          Assert.IsNotNull(_defaultCommunicationNet.Relations);
+          Assert.IsNotNull(_defaultCommunicationNet.Relations);
+          Assert.IsNotNull(_defaultCommunicationNet.Segments);
+          Assert.IsNotNull(_defaultCommunicationNet.SerialSetings);
+          Assert.IsNotNull(_defaultCommunicationNet.Station);
+          Assert.IsNotNull(_defaultCommunicationNet.TagBit);
+          Assert.IsNotNull(_defaultCommunicationNet.Tags);
+        }
+        Assert.IsNull(_new.Container);
+        Assert.AreEqual<string>(@"CommServerConfiguration", _new.DefaultFileName);
+        Assert.IsNotNull(_new.Menu);
+        Assert.IsNotNull(_new.OpenFileDialog);
+      }
+    }
+  }
+}
---
+++ ProtocolHub.ConfiguartionEditor.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProtocolHub.ConfiguartionEditor.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ProtocolHub.ConfiguartionEditor.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65ede546-73e7-4434-84da-83d2cc0efddb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
